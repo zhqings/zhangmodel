9008345f4c5b1630bf30a1aa8890fa9ec65c891f
haiqing.zhang <zhangtopsun@163.com>
2021-07-12 17:45:46 +0800

    first commit

---
+++ src/main/java/com/study/zhq/ZhqApplication.java
@@ -0,0 +1,13 @@
+package com.study.zhq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhqApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ZhqApplication.class, args);
+    }
+    
+}
